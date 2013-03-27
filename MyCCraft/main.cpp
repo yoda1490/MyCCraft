c66653b2fc5670250ca01176e04b11e2f5a88c26
yoda1490 <y0da@live.fr>
2013-03-27 23:57:00 +0700

    Initial Commit

---
+++ MyCCraft/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyCCraft
+//
+//  Created by WaWa-YoDa on 27/03/13.
+//  Copyright (c) 2013 WaWa-YoDa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
